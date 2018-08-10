3037f265fbcec33370daad507169592b85cd8e8d
stefan_sentic <stefan.sentic@htecgroup.com>
2018-08-10 16:17:33 +0200

    launch modes test activities

--- app/src/main/java/com/android/htec/vezba/MainActivity.java
+++ app/src/main/java/com/android/htec/vezba/MainActivity.java
@@ -1,12 +1,15 @@
 package com.android.htec.vezba;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.database.DatabaseUtils;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.android.htec.vezba.databinding.ActivityMainBinding;
+import com.android.htec.vezba.launchmode.LaunchModesActivity;
 import com.android.htec.vezba.viewmodel.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,4 +25,19 @@ public class MainActivity extends AppCompatActivity {
         binding.setViewModel(viewModel);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuitem_launch_modes:
+                startActivity(new Intent(this, LaunchModesActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/com/android/htec/vezba/launchmode/LaunchModesActivity.java
@@ -0,0 +1,26 @@
+package com.android.htec.vezba.launchmode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.htec.vezba.R;
+
+public class LaunchModesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_mode);
+        initButtons();
+    }
+
+    private void initButtons() {
+        findViewById(R.id.btn_standard).setOnClickListener(v -> startActivity(new Intent(LaunchModesActivity.this, LaunchModesActivity.class)));
+        findViewById(R.id.btn_single_top).setOnClickListener(v -> startActivity(new Intent(LaunchModesActivity.this, SingleTopActivity.class)));
+        findViewById(R.id.btn_single_task).setOnClickListener(v -> startActivity(new Intent(LaunchModesActivity.this, SingleTaskActivity.class)));
+        findViewById(R.id.btn_single_instance).setOnClickListener(v -> startActivity(new Intent(LaunchModesActivity.this, SingleInstanceActivity.class)));
+    }
+
+}
