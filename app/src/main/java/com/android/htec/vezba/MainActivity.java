71c31cad3f4904205d337345d286e94f481eadee
stefan_sentic <stefan.sentic@htecgroup.com>
2018-08-10 11:09:40 +0200

    view model survival test

--- app/src/main/java/com/android/htec/vezba/MainActivity.java
+++ app/src/main/java/com/android/htec/vezba/MainActivity.java
@@ -1,14 +1,25 @@
 package com.android.htec.vezba;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.database.DatabaseUtils;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.htec.vezba.databinding.ActivityMainBinding;
+import com.android.htec.vezba.viewmodel.MainActivityViewModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MainActivityViewModel viewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        binding.setViewModel(viewModel);
     }
 
 }
--- app/src/main/java/com/android/htec/vezba/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/android/htec/vezba/viewmodel/MainActivityViewModel.java
@@ -1,8 +1,18 @@
 package com.android.htec.vezba.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
 
 public class MainActivityViewModel extends ViewModel {
 
+    private ObservableField<String> countValue = new ObservableField<>("0");
 
+
+    public void onButtonIncrementClick() {
+        countValue.set(String.valueOf(Integer.valueOf(countValue.get()) + 1));
+    }
+
+    public ObservableField<String> getCountValue() {
+        return countValue;
+    }
 }
