b8c4bc5e938eadbde8da70eada1741d0e8596c11
stefans2708 <stefan.s27.08@gmail.com>
2018-08-12 14:58:36 +0200

    custom toolbar finshed

--- app/src/main/java/com/android/htec/vezba/MainActivity.java
+++ app/src/main/java/com/android/htec/vezba/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
         binding.setViewModel(viewModel);
+        Toolbar toolbar = (Toolbar) binding.appBar;
+        setSupportActionBar(toolbar);
     }
 
     @Override
