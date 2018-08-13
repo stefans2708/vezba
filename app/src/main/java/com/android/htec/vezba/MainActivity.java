cc49c2d98d8f13c498ca2f79fd56e17c7d08c450
stefan_sentic <stefan.sentic@htecgroup.com>
2018-08-13 12:37:52 +0200

    custom dialog

--- app/src/main/java/com/android/htec/vezba/MainActivity.java
+++ app/src/main/java/com/android/htec/vezba/MainActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.android.htec.vezba.databinding.ActivityMainBinding;
+import com.android.htec.vezba.dialog.ImageDialogFragment;
 import com.android.htec.vezba.launchmode.LaunchModesActivity;
 import com.android.htec.vezba.viewmodel.MainActivityViewModel;
 
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
         binding.setViewModel(viewModel);
+        setSupportActionBar(binding.appBar.findViewById(R.id.toolbar));
     }
 
     @Override
@@ -36,8 +39,11 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.menuitem_launch_modes:
                 startActivity(new Intent(this, LaunchModesActivity.class));
+            case R.id.menuitem_dialogs:
+                new ImageDialogFragment().show(getFragmentManager(), "tag");
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
